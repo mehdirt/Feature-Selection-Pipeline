c93053c7cd581aaa06fd5cc2c5b310d3c18aa019
mehdirt <mahdirafati680@gmail.com>
2024-10-21 10:55:15 +0330

    Feat: Lasso method implemented

--- selection_methods/lasso_method.py
+++ selection_methods/lasso_method.py
@@ -1,6 +1,5 @@
 import numpy as np
 from sklearn.feature_selection import SelectFromModel
-from sklearn.model_selection import RepeatedKFold
 from sklearn.preprocessing import StandardScaler
 from sklearn.pipeline import Pipeline
 from sklearn.linear_model import Lasso
@@ -8,30 +7,38 @@ from sklearn.linear_model import Lasso
 from .base_method import FeatureSelectionMethod
 
 class LassoMethod(FeatureSelectionMethod):
-    def __init__():
-        pass
+    def __init__(self, n_features, alpha, max_iter):
+        super().__init__(self, n_features)
+        self.alpha = alpha
+        self.max_iter = max_iter
 
     def fit(self, X, y=None):
-        must = []
+        metas = list(X.columns)
 
         pipeline = Pipeline([
         ('scaler', StandardScaler()),  # Step 1: Scaling
-        ('lasso', Lasso(alpha=self.args.alpha, self.args.max_iter))  # Step 2: Lasso regression with alpha
+        ('lasso', Lasso(alpha=self.alpha, max_iter=self.max_iter))  # Step 2: Lasso regression with alpha
         ])
 
         pipeline.fit(X, y)
         cls = pipeline.named_steps['lasso']
-        imp = np.sort(np.abs(cls.coef_))[-self.args.k]
-        print("imp:", imp)
-        sel = SelectFromModel(cls, prefit=True, max_features=self.args.k)
-        print("sel:",sel)
-        self.mb_ = np.array(list(range(X.shape[1])))[sel.get_support()]
-        print("self.mb_:",self.mb_)
-        if len(self.mb_) >= self.args.k:
-            print("over max_features", self.mb_)
-        if isinstance(self.mb_,list):
-            self.mb_ = sorted([i for i in self.mb_ if i not in must])
-            print(self.mb_)
+        
+        # imp = np.sort(np.abs(cls.coef_))[-self.n_features]
+        # print("imp:", imp)
+        
+        # Finding remained features
+        sel = SelectFromModel(cls, prefit=True, max_features=self.n_features)
+        # print("sel:",sel)
+
+        mb_ids = np.array(list(range(X.shape[1])))[sel.get_support()] # Metabolites' indices
+        # print("self.mb_:", self.mb_)
+        self.mb_ = [metas[i] for i in mb_ids] # Metabolites' names
+        # if len(self.mb_) >= self.n_features:
+        #     print("over max_features", self.mb_)
+
+        # if isinstance(self.mb_, list):
+        #     self.mb_ = sorted([i for i in self.mb_ if i not in must])
+        #     print(self.mb_)
         return self
     
     def transform(self, X):
