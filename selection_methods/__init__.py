0b2791d0e47b4cd5cd19b0d49168ecc9bc8a74da
mehdirt <mahdirafati680@gmail.com>
2024-10-21 15:47:11 +0330

    Fear: __init__.py fixed

---
+++ selection_methods/__init__.py
@@ -0,0 +1,10 @@
+__all__ = ['alasso_method', 'base_method', 'boruta_method', 'elasticnet_method', 'lasso_method', 'mlrrf_method', 'relieff_method', 'svmrfe_method']
+
+from . import base_method
+from . import alasso_method
+from . import boruta_method
+from . import elasticnet_method
+from . import lasso_method
+from . import mlrrf_method
+from . import relieff_method
+from . import svmrfe_method
