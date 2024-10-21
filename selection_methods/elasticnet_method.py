c458ea7e7ad60d0dfa11cbc3e9c3ae8616a6b720
mehdirt <mahdirafati680@gmail.com>
2024-10-21 12:08:42 +0330

    Fear: Elastic-Net method implemented

--- selection_methods/elasticnet_method.py
+++ selection_methods/elasticnet_method.py
@@ -1,15 +1,16 @@
 import numpy as np
 from sklearn.feature_selection import SelectFromModel
 from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import ElasticNet
 from sklearn.pipeline import Pipeline
-from sklearn.linear_model import Lasso
 # Import base class
 from .base_method import FeatureSelectionMethod
 
 class LassoMethod(FeatureSelectionMethod):
-    def __init__(self, n_features, alpha=1.0, max_iter=1000):
+    def __init__(self, n_features, alpha=1.0, l1_ratio=0.5, max_iter=1000):
         super().__init__(self, n_features)
         self.alpha = alpha
+        self.l1_ratio = l1_ratio
         self.max_iter = max_iter
 
     def fit(self, X, y=None):
@@ -17,14 +18,14 @@ class LassoMethod(FeatureSelectionMethod):
 
         pipeline = Pipeline([
         ('scaler', StandardScaler()),  # Scaling
-        ('lasso', Lasso(alpha=self.alpha, max_iter=self.max_iter))  # Lasso regression with alpha
+        ('elasticnet', ElasticNet(alpha=self.alpha, l1_ratio=self.l1_ratio, max_iter=self.max_iter))  # ElasticNet regression 
         ])
 
         pipeline.fit(X, y)
-        cls = pipeline.named_steps['lasso']
+        cls = pipeline.named_steps['elasticnet']
         
-        # imp = np.sort(np.abs(cls.coef_))[-self.n_features]
-        # print("imp:", imp)
+        # imp = np.sort(np.abs(cls.coef_))[-self.args.k]
+        # print("imp:",imp)
         
         # Finding remained features
         sel = SelectFromModel(cls, prefit=True, max_features=self.n_features)
@@ -32,6 +33,7 @@ class LassoMethod(FeatureSelectionMethod):
 
         mb_ids = np.array(list(range(X.shape[1])))[sel.get_support()] # Metabolites' indices
         # print("self.mb_:", self.mb_)
+        
         self.mb_ = [metas[i] for i in mb_ids] # Metabolites' names
         # if len(self.mb_) >= self.n_features:
         #     print("over max_features", self.mb_)
