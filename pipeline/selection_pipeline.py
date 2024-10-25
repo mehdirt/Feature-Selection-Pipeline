d591878b5186476f67e41a48ce2e5da8fee1b29b
mehdirt <mahdirafati680@gmail.com>
2024-10-25 15:42:28 +0330

    Feat: Added exluded metas in Selection Pipeline

--- pipeline/selection_pipeline.py
+++ pipeline/selection_pipeline.py
@@ -2,6 +2,7 @@ class SelectionPipeline:
     def __init__(self):
         self.methods = []
         self.method_metas = {}
+        self.exclude_metas = ['Glycerate-2P_Glycerate-3P_neg-006','Citraconic acid_neg-025','Pyridoxine_pos-137','Argininosuccinic acid_pos-039']
 
     def add_method(self, method):
         self.methods.append(method)
@@ -9,6 +10,6 @@ class SelectionPipeline:
     def apply(self, X, y):
         for method in self.methods:
             method.fit(X, y)
-            self.method_metas[f"{method.__class__.__name__}"] = method.mb_
+            self.method_metas[f"{method.__class__.__name__}"] = [met for met in method.mb_ if met not in self.exclude_metas]
         
         return self.method_metas
