f08d22135e1d10458af56318a3a161d030003cc6
Mahdi Rafati <mahdirafati680@gmail.com>
2025-04-20 21:21:58 +0330

    Docs: FS class moved to its own folder: core

---
+++ core/fs/__init__.py
@@ -0,0 +1,8 @@
+"""
+Feature Selection (FS) package for metabolomics data analysis.
+This package provides the main FS class and related functionality for feature selection.
+"""
+
+from .fs import FS
+
+__all__ = ['FS'] 
